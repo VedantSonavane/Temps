8ad6a0f8c902645b8235554e1a5243cd3ddf27be
VedantSonavane <92395669+VedantSonavane@users.noreply.github.com>
2024-12-01 07:17:34 +0000

    1st dec

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,36 @@
-import { Hono } from "hono";
-const app = new Hono();
+import { Hono } from 'hono'
 
-app.get("/status", (c) => {
-  return c.json({ message:"server is running successfully" });
-});
+const app = new Hono()
+let readingTime=238;
 
-app.get("/", (c) => {
-  return c.html(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Temps</title>
-      </head>
-      <body>
-        <h1>Temps</h1>
-        <p>Calculate your time</p>
-        <form action="/calculate" method="GET">
-          <label for="text">Text</label>
-          <input type="text" name="text" id="text" />
-          <label for="wpm">Words per minute</label>
-          <input type="number" name="wpm" id="wpm" />
-          <button type="submit">Calculate</button>
-        </form>
-      </body>
-    </html>
-  `);
-});
+function calculatereadingTime(sentence: string, wpm: number) {
+  const wordscount = sentence.split(/\s+/).length
+  const minutes = wordscount / wpm
+  const seconds = minutes * 60
+  return {wordscount, minutes, seconds,wpm}
+}
+let testStr = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatum incidunt nemo minus dicta ratione a nam voluptatem maxime, deleniti necessitatibus provident porro quibusdam, veniam atque qui. Quas architecto culpa, perferendis beatae officia dicta aliquid veritatis voluptatibus, est modi incidunt doloribus vel sunt exercitationem ab odio. Natus velit ipsum sed similique numquam, eum officiis asperiores nesciunt quibusdam eveniet eaque amet voluptatum repellendus consequatur omnis debitis odit neque iusto minus possimus nostrum vel. Doloribus voluptatum enim vel eum nesciunt consectetur, deserunt aspernatur labore odio at! Reiciendis soluta voluptatibus veritatis est qui, exercitationem rerum sed quaerat, inventore neque tenetur enim debitis quo voluptas consequatur explicabo. Ea eius ad sed provident, exercitationem fugiat nulla distinctio voluptate eligendi perspiciatis ipsum quidem illum voluptates labore quibusdam rem! Modi dignissimos distinctio voluptatem in officia natus aliquam voluptates praesentium magni quo totam aliquid dolor obcaecati enim quae repudiandae consequuntur recusandae earum omnis quisquam sunt consequatur, a nisi adipisci? Laboriosam, culpa nihil ipsa optio eaque eveniet pariatur at repudiandae sint ex laudantium id assumenda illo nostrum nobis dolorum corrupti nulla enim amet eius. Reiciendis, veniam. Quo, voluptatibus, doloremque veritatis a placeat incidunt nam eaque unde quaerat iusto debitis delectus error explicabo earum nisi sint cum harum architecto vero. Maxime autem quaerat culpa voluptas minima tempora hic est quidem repellendus praesentium? Placeat consequuntur suscipit cupiditate voluptate excepturi deleniti amet? Nemo, distinctio deleniti corporis voluptas provident dolorem aliquid, dolores at officiis, aspernatur eos pariatur porro repudiandae laborum necessitatibus!";
 
-app.get("/calculate", (c) => {
-    const text = c.req.query("text");
-    const wpm = c.req.query("wpm");
-    return c.json(  
-        { message:text,wpm }  
-    );
-});
+const x = calculatereadingTime(testStr, readingTime);
+console.log(x);
 
-app.get("/calculate", (c) => {
-  const text =c.req.query("text");
-  const wpm = c.req.query("wpm");
-  return c.json({ message:text,wpm });
-});
+app.get('/', (c) => {
+ const sentence = c.req.query("sentence")
+ const wpm = c.req.query("wpm")
 
-export default app;
+ if (!sentence) {
+   return c.text('Please provide a sentence', 400)
+ }
+
+ if (!wpm) {
+   return c.text('Please provide a wpm')
+ }
+
+ console.log(sentence);
+ return c.json({
+   sentence,
+   wpm,
+ })
+})
+
+export default app
